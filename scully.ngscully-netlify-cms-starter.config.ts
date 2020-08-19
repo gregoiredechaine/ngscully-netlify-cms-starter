31c74cc7d8e8c5cf07ad1407d480e9371c0b283b
gregoiredechaine <gregdechaine@me.com>
2020-08-19 14:26:52 -0400

    Some changes

--- scully.ngscully-netlify-cms-starter.config.ts
+++ scully.ngscully-netlify-cms-starter.config.ts
@@ -9,6 +9,11 @@ export const config: ScullyConfig = {
       slug: {
         folder: './content/blog',
       },
+      '/directory/:slug': {
+        type: 'contentFolder',
+        slug: {
+          folder: './content/directory',
+        },
     },
   },
 };
