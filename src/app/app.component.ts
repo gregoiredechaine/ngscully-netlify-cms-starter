6720399879eb907098099eabeadb81d54ee8a241
gregoiredechaine <gregdechaine@me.com>
2020-08-25 10:30:13 -0400

    Refactor padding and add Google Analytics

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { IdleMonitorService } from '@scullyio/ng-lib';
+import { Router, NavigationEnd } from '@angular/router';
 
 import { ThemeService } from './core/services/theme.service';
 import { CustomIconService } from './core/services/custom-icon.service';
+
+declare let gtag: Function;
 
 @Component({
   selector: 'app-root',
@@ -16,9 +19,19 @@ export class AppComponent implements OnInit {
 
  constructor (
    private idle: IdleMonitorService,
+   public router: Router,
    private themeService: ThemeService,
    private customIconService: CustomIconService) {
      this.customIconService.init();
+     this.router.events.subscribe(event => {
+         if(event instanceof NavigationEnd){
+            gtag('config', 'G-9QY8E40399',
+                   {
+                     'page_path': event.urlAfterRedirects
+                   }
+                  );
+          }
+      });
    }
 
  ngOnInit() {
